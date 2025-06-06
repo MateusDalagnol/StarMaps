a421beede67165633536f905ab51f6723dd64660
MateusDalagnol <dalagnolm@gmail.com>
2025-06-06 10:54:31 -0300

    leitura de dados do arquivo

--- GrafosPlanetas.c
+++ GrafosPlanetas.c
@@ -1,46 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define SS 9 //Planetas incluindo o sol
+#define MAX 9 //Planetas incluindo o sol
 
-void inserirAresta(double matriz[][SS], int origem, int destino, double custo){
+void inserirAresta(double matriz[][MAX], int origem, int destino, double custo){
     matriz[origem][destino] = custo;
     matriz[destino][origem] = custo;
 }
 
-void removerAresta(double matriz[][SS], int origem, int destino){
+void removerAresta(double matriz[][MAX], int origem, int destino){
     matriz[origem][destino] = -1;
     matriz[destino][origem] = -1;
 }
 
-int main(){
-
-    double matriz[SS][SS];
-    
-    for (int i = 0; i < SS; i++) {
-        for (int j = 0; j < SS; j++) {
+void inicializa_matriz(double matriz[][MAX]){
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
             matriz[i][j] = -1;
         }
     }
+}
 
-    inserirAresta(matriz, 4,8,40.1);
-    inserirAresta(matriz, 5,1,60.5);
-    inserirAresta(matriz, 0,3,60.8);
-    inserirAresta(matriz, 3,7,4.32);
+void le_arquivo(double matriz[][MAX], FILE *arquivo){
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
+            if (fscanf(arquivo, "%lf", &matriz[i][j]) != 1) {
+                printf("Erro ao ler [%d][%d]\n", i, j);
+                matriz[i][j] = -1;
+            }
+        }
+    }
+}
 
-
-    printf("");
-
+void imprime_matriz(double matriz[][MAX]){
     printf("\nMatriz de custos:\n");
-    for (int i = 0; i < SS; i++) {
-        for (int j = 0; j < SS; j++) {
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
             if (matriz[i][j] == -1)
                 printf(" -- ");
             else
-                printf("%2.1lf ", matriz[i][j]);
+                printf(" %2.1lf ", matriz[i][j]);
         }
         printf("\n");
     }
+}
+
+int main(){
+
+    FILE *arquivo;
+
+    arquivo = fopen("custoPlanetas.txt", "r");
+
+    if (arquivo == NULL) {
+    printf("Arquivo nao foi aberto corretamente!\n");
+    return 1;
+}
+
+    char* planetas[MAX] = {"Sol", "Mercurio", "Venus", "Terra", "Marte", "Jupiter",
+         "Saturno", "Urano", "Netuno"};
+    double matriz[MAX][MAX];
+    
+    inicializa_matriz(matriz);
+
+    le_arquivo(matriz, arquivo);
+
+    imprime_matriz(matriz);
+
+    if (fclose(arquivo) != 0) {
+        printf("Erro ao fechar o arquivo!\n");
+    }
+
     return 0;
 
 }
